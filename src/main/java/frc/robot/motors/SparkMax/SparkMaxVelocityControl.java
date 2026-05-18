7ee774b6adc6ae6e760c7a3adbf6ba8f432d3810
Timothy Shu <tims11105@gmail.com>
2026-05-18 16:33:41 +1000

    fixed

--- src/main/java/frc/robot/motors/SparkMax/SparkMaxVelocityControl.java
+++ src/main/java/frc/robot/motors/SparkMax/SparkMaxVelocityControl.java
@@ -40,7 +40,7 @@ public class SparkMaxVelocityControl extends SparkMaxBase implements IMotorVeloc
      * @param kA the acceleration gain for the feedforward controller
      * @return
      */
-    public static IMotorVelocityControl CreateSparkMaxVelocityontroller(
+    public static IMotorVelocityControl CreateSparkMaxVelocityController(
         int deviceID,
         MotorType type,
         boolean isInverted,
