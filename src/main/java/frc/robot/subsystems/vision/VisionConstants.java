a587da9c983ba99b03aa23585f33a4062e1695fb
Gamechecker6745 <133551146+Gamechecker6745@users.noreply.github.com>
2026-06-09 16:26:06 +1000

    Update VisionConstants.java

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -3,20 +3,20 @@ package frc.robot.subsystems.vision;
 public class VisionConstants {
 
     public static final class CameraLeft {
-        public static final double X     = -0.273;
-        public static final double Y     =  0.252;
-        public static final double Z     =  0.235;
-        public static final double PITCH = 10 * Math.PI / 180;
-        public static final double ROLL  = 0;
-        public static final double YAW   = 169.5 * Math.PI / 180;
+        public static final double X     = -0.273; // Uncalibrated
+        public static final double Y     =  0.252; // Uncalibrated
+        public static final double Z     =  0.235; // Uncalibrated
+        public static final double PITCH = 10 * Math.PI / 180; // Uncalibrated
+        public static final double ROLL  = 0; // Uncalibrated
+        public static final double YAW   = 169.5 * Math.PI / 180; // Uncalibrated
     }
 
     public static final class CameraRight {
-        public static final double X     = -0.27;
-        public static final double Y     = -0.249;
-        public static final double Z     =  0.235;
-        public static final double PITCH = 10 * Math.PI / 180;
-        public static final double ROLL  = 0;
-        public static final double YAW   = 190.5 * Math.PI / 180;
+        public static final double X     = -0.27; // Uncalibrated
+        public static final double Y     = -0.249; // Uncalibrated
+        public static final double Z     =  0.235; // Uncalibrated
+        public static final double PITCH = 10 * Math.PI / 180; // Uncalibrated
+        public static final double ROLL  = 180 * Math.PI / 180;
+        public static final double YAW   = 190.5 * Math.PI / 180; // Uncalibrated
     }
 }
